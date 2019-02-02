3cf50096d86f056308ed1ccfd6407914923be8d4
Tony Hirst <tony.hirst@gmail.com>
2019-02-02 01:06:17 +0000

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,6 +1,6 @@
 c.ServerProxy.servers = {
     'drill': {
-        #this probably won't work because it wants a terminal
+        #this probably won't work because it wants a terminal - to run as a daemon, requires zookeeper...
         #Also, there is no port option?
         'command': ['/home/jovyan/apache-drill-1.15.0/bin/drill-embedded'],
         'timeout': 120,
