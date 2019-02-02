28e403aeb69ea4136f4fb567f64d69147e12dacb
Tony Hirst <tony.hirst@gmail.com>
2019-02-02 01:06:49 +0000

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,7 +1,7 @@
 c.ServerProxy.servers = {
     'drill': {
         #this probably won't work because it wants a terminal - to run as a daemon, requires zookeeper...
-        #Also, there is no port option?
+        #Also, there is no port option? Or can we prefix start command with an env var to specifiy the port?
         'command': ['/home/jovyan/apache-drill-1.15.0/bin/drill-embedded'],
         'timeout': 120,
         'absolute_url': True, 
